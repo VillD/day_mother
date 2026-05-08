60666fefb94f7d5359feadfbf006cd8ab55db9fe
Daniel Rodriguez <rodriguezvillegasdl@gmail.com>
2026-05-08 16:01:40 -0500

    update dist routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/mother',
 	site: 'https://feliz-dia-mama-harvar.netlify.app/',
+	base: '/mother',
 	vite: {
 		plugins: [tailwindcss()],
 	},
