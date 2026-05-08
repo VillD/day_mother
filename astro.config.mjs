3cf50aca3add387b13aa87f64ad775eca2b7096f
Daniel Rodriguez <rodriguezvillegasdl@gmail.com>
2026-05-08 16:05:31 -0500

    restaura rutas dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://feliz-dia-mama-harvar.netlify.app',
-	base: '/mother',
 	vite: {
 		plugins: [tailwindcss()],
 	},
