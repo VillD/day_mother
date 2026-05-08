f9cc774473174fd3eca6a9cb1a5065897e8aa3ec
Daniel Rodriguez <rodriguezvillegasdl@gmail.com>
2026-05-08 15:53:19 -0500

    update dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+	base: '/mother',
+	site: 'https://feliz-dia-mama-harvar.netlify.app/',
+	vite: {
+		plugins: [tailwindcss()],
+	},
+})
